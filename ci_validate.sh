f949d8758bb3255276f78f75747eab2e221a617e
Dale Alleshouse <dalealleshouse@gmail.com>
2018-08-19 09:24:30 -0400

    experiment with travis

--- ci_validate.sh
+++ ci_validate.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cd sorting/c
+ls
+cd dalealleshouse/algorithms/sorting/c
 docker run --privileged --rm -v $(pwd):/src dalealleshouse/algo_test_runner_c \
   ./validate.sh > run_result.txt
 
