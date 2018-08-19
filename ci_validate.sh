41da81a350267be72bc34fd241df7e6af0809c0e
Dale Alleshouse <dalealleshouse@gmail.com>
2018-08-19 09:59:31 -0400

    experimenting with travis

--- ci_validate.sh
+++ ci_validate.sh
@@ -1,8 +1,33 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-ls
-# cd dalealleshouse/algorithms/sorting/c
-# docker run --privileged --rm -v $(pwd):/src dalealleshouse/algo_test_runner_c \
-#   ./validate.sh > run_result.txt
+function run_tests() {
+  docker run --privileged --rm -v $(pwd):/src dalealleshouse/algo_test_runner_c \
+    ./validate.sh > run_result.txt
 
-# cat run_result.txt
+  RESULTS=`tail run_result.txt`
+
+  if [[ "$RESULTS" == *Success* ]]
+  then
+    echo `pwd` passed
+  else
+    echo `pwd` failure
+    exit -1
+  fi
+}
+
+cd closest_pair/c/
+run_tests
+
+cd ../../inversion_count/c
+run_tests
+
+cd ../../quick_select/c
+run_tests
+
+cd ../../sorting/c
+run_tests
+
+cd ../../strassen_matrix_multiplication/c
+run_tests
+
+exit 0
