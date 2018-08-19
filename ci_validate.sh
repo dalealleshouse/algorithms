bcb836da61b647b154491d3496747de9dd133d19
Dale Alleshouse <dalealleshouse@gmail.com>
2018-08-19 12:01:50 -0400

    simplified ci validation script

--- ci_validate.sh
+++ ci_validate.sh
@@ -1,33 +1,26 @@
 #!/usr/bin/env bash
 
-function run_tests() {
-  docker run --privileged --rm -v $(pwd):/src dalealleshouse/algo_test_runner_c \
-    ./validate.sh > run_result.txt
+function run_c_tests() {
+    docker run --privileged --rm -v $(pwd):/src dalealleshouse/algo_test_runner_c \
+        ./validate.sh > run_result.txt
 
-  RESULTS=`tail run_result.txt`
+    RESULTS=`tail run_result.txt`
 
-  if [[ "$RESULTS" == *Success* ]]
-  then
-    echo `pwd` passed
-  else
-    echo `pwd` failure
-    exit -1
-  fi
+    if [[ "$RESULTS" == *Success* ]]
+    then
+        echo `pwd` passed
+    else
+        echo `pwd` failure
+        exit -1
+    fi
 }
 
-cd closest_pair/c/
-run_tests
-
-cd ../../inversion_count/c
-run_tests
-
-cd ../../quick_select/c
-run_tests
-
-cd ../../sorting/c
-run_tests
-
-cd ../../strassen_matrix_multiplication/c
-run_tests
+# Run the dockerized tests inside every subdirectory named c
+for D in `find . -type d -name 'c' ! -path './docker/c'`
+do
+    cd $D
+    run_c_tests
+    cd ../../
+done
 
 exit 0
