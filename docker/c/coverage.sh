99e16cd6782a27e1e6fbff539c69e9cb3e0b149c
Dale Alleshouse <dalealleshouse@gmail.com>
2018-08-20 17:57:18 -0400

    still trying to fix code coverage

--- docker/c/coverage.sh
+++ docker/c/coverage.sh
@@ -2,7 +2,14 @@
 
 make -B code-coverage > /dev/null
 
-for F in `find ./ -name "*.c" ! -name "*test*" ! -name "algo*"`
+for F in `find ./ -name "*.gcda" ! -name "*test*" ! -name "algo*"`
 do
-    llvm-cov gcov $F -n
+    llvm-cov gcov -f -b $F
 done
+
+lcov --directory ./src \
+    --base-directory ./src \
+    --gcov-tool ./llvm-gcov.sh \
+    --capture -o ./src/cov.info
+
+genhtml ./src/cov.info -o ./src/output
