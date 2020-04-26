8cb4527510f57336022d7e5700f71b7c1ec101aa
Dale Alleshouse <dalealleshouse@gmail.com>
2020-04-26 14:02:14 -0400

    WIP

---
+++ satisfiability/c/Satisfiability.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdbool.h>
+
+#include "include/ResultCode.h"
+
+typedef long variable;
+typedef struct clause {
+  variable a;
+  variable b;
+} clause;
+
+ResultCode Sat_EvalClause(clause* clause, bool a, bool b, bool* satified);
+ResultCode Sat_Solve(size_t n, clause clauses[n], bool* result);
