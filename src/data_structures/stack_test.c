e63a7f99409bfa632c71d3852c2c2ee559155a76
Dale Alleshouse <dalealleshouse@gmail.com>
2020-10-11 07:01:59 -0400

    fixed minor formatting issue

--- src/data_structures/stack_test.c
+++ src/data_structures/stack_test.c
@@ -1,11 +1,11 @@
-  /*******************************************************************************
-  * Copyright (C) 2020 Dale Alleshouse (AKA Hideous Humpback Freak)
-  *  dale@alleshouse.net https://hideoushumpbackfreak.com/
-  *
-  * This file is subject to the terms and conditions defined in the 'LICENSE'
-  * file, which is part of this source code package.
-  ******************************************************************************/
-#include "./stack.h"
+/*******************************************************************************
+ * Copyright (C) 2020 Dale Alleshouse (AKA Hideous Humpback Freak)
+ *  dale@alleshouse.net https://hideoushumpbackfreak.com/
+ *
+ * This file is subject to the terms and conditions defined in the 'LICENSE'
+ * file, which is part of this source code package.
+ ******************************************************************************/
+#include "stack.h"
 
 #include <stdlib.h>
 
