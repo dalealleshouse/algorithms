9f64bfc4c72933d62aa397cd26c4eb4d569e7bf5
Dale Alleshouse <dalealleshouse@gmail.com>
2020-11-02 09:41:56 -0500

    updated bloom filter to look more like the supplied pseudocode

--- src/data_structures/bloom_filter.c
+++ src/data_structures/bloom_filter.c
@@ -11,37 +11,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "common_math.h"
 #include "hash_functions.h"
 
-static const size_t byte_size = 8;
+static const size_t kByteSize = 8;
 
 typedef struct BloomFilter {
   size_t n;
   size_t bits;
-  size_t funcs;
   char* filter;
+  size_t hasher_count;
+  const hasher* hashers;
 } BloomFilter;
 
-static bool is_power_2(size_t n) { return (ceil(log2(n)) == floor(log2(n))); }
-
-ResultCode BloomFilter_Create(size_t bits, size_t funcs, BloomFilter** self) {
-  if (bits < byte_size || !is_power_2(bits) || funcs == 0 ||
-      funcs > hasher_count) {
+ResultCode BloomFilter_Create(size_t bits, const hasher hashers[],
+                              const size_t hasher_count, BloomFilter** self) {
+  if (bits < kByteSize || hasher_count == 0) {
     return kArgumentOutOfRange;
   }
 
   BloomFilter* filter = malloc(sizeof(BloomFilter));
   if (filter == NULL) return kFailedMemoryAllocation;
 
-  filter->filter = calloc(1, bits / byte_size);
+  size_t actual_bits = NextPowerOfTwo(bits);
+  filter->filter = calloc(1, actual_bits / kByteSize);
   if (filter->filter == NULL) {
     BloomFilter_Destroy(filter);
     return kFailedMemoryAllocation;
   }
 
   filter->n = 0;
-  filter->bits = bits;
-  filter->funcs = funcs;
+  filter->bits = actual_bits;
+  filter->hasher_count = hasher_count;
+  filter->hashers = hashers;
 
   *self = filter;
   return kSuccess;
@@ -53,16 +55,15 @@ size_t BloomFilter_GetN(const BloomFilter* self) {
   return self->n;
 }
 
-ResultCode BloomFilter_Insert(BloomFilter* self, const char* key) {
+ResultCode BloomFilter_Insert(BloomFilter* self, const void* key,
+                              size_t key_size) {
   if (self == NULL || key == NULL) return kNullParameter;
 
-  size_t len = strlen(key);
-
-  for (size_t i = 0; i < self->funcs; i++) {
-    hash hash = hashers[i]((void*)key, len);
+  for (size_t i = 0; i < self->hasher_count; i++) {
+    hash hash = hashers[i]((void*)key, key_size);
     size_t index = mul_compressor64(hash, self->bits);
-    size_t byte_index = index / byte_size;
-    size_t bit_index = index % byte_size;
+    size_t byte_index = index / kByteSize;
+    size_t bit_index = index % kByteSize;
 
     self->filter[byte_index] =
         (char)(self->filter[byte_index] | 1 << bit_index);
@@ -72,17 +73,15 @@ ResultCode BloomFilter_Insert(BloomFilter* self, const char* key) {
   return kSuccess;
 }
 
-ResultCode BloomFilter_Lookup(const BloomFilter* self, const char* key,
-                              bool* result) {
+ResultCode BloomFilter_Lookup(const BloomFilter* self, const void* key,
+                              size_t key_size, bool* result) {
   if (self == NULL || key == NULL) return kNullParameter;
 
-  size_t len = strlen(key);
-
-  for (size_t i = 0; i < self->funcs; i++) {
-    hash hash = hashers[i]((void*)key, len);
+  for (size_t i = 0; i < self->hasher_count; i++) {
+    hash hash = hashers[i]((void*)key, key_size);
     size_t index = mul_compressor64(hash, self->bits);
-    size_t byte_index = index / byte_size;
-    size_t bit_index = index % byte_size;
+    size_t byte_index = index / kByteSize;
+    size_t bit_index = index % kByteSize;
 
     if (!(self->filter[byte_index] & 1 << bit_index)) {
       *result = false;
