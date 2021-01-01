c83053e56f836a84922a7cc5aabc05db509a066b
Dale Alleshouse <dalealleshouse@gmail.com>
2021-01-01 11:08:11 -0500

    remove ulimit from docker run because it's not compatible with the build server

--- run_c.sh
+++ run_c.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# add this line into the docker run command to increase the stack size. This
+# is useful for testing large inputs on recursive algorithms.
+# --ulimit stack=8277716992:8277716992 \
+
 docker run --privileged --rm --tty -it \
     -v $(pwd):/build \
     --user "$(id -u):$(id -g)" \
-    --ulimit stack=8277716992:8277716992 \
     dalealleshouse/algo_test_runner_c:latest \
     $1
