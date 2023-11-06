474eb4b076d85833efced04b8bcc3d94c5b0e731
Dale Alleshouse <dalealleshouse@gmail.com>
2023-11-06 07:48:51 -0500

    run in either podman or docker

--- run_c.sh
+++ run_c.sh
@@ -4,7 +4,39 @@
 # is useful for testing large inputs on recursive algorithms.
 # --ulimit stack=8277716992:8277716992 \
 
-podman run --privileged --rm --tty -it \
-    -v $(pwd):/build \
-    dalealleshouse/algo_test_runner_c:4.0 \
-    $1
+# Check if arguments are provided
+if [[ $# -eq 0 ]]; then
+  echo 'Please provide a version number e.g.:'
+  echo './build_container.sh 1.1'
+  exit 0
+fi
+
+# Check if podman or docker is installed
+if command -v podman &> /dev/null; then
+  CONTAINER_TOOL="podman"
+elif command -v docker &> /dev/null; then
+  CONTAINER_TOOL="docker"
+else
+  echo "Error: Neither podman nor docker is installed."
+  exit 1
+fi
+
+case "$1" in
+  push) $CONTAINER_TOOL push dalealleshouse/algo_test_runner_c ;;
+  *) $CONTAINER_TOOL build -t dalealleshouse/algo_test_runner_c:$1 \
+       -t dalealleshouse/algo_test_runner_c \
+       ./
+     $CONTAINER_TOOL system prune -f ;;
+esac
+
+
+# podman run --privileged --rm --tty -it \
+#     -v $(pwd):/build \
+#     dalealleshouse/algo_test_runner_c:4.0 \
+#     $1
+
+# docker run --privileged --rm --tty -it \
+#     -v $(pwd):/build \
+#     --user "$(id -u):$(id -g)" \
+#     dalealleshouse/algo_test_runner_c:latest \
+#     $1
